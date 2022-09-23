65af3c1e697f59754d7b1ff37290e8b1b932e91a
Hossamb1 <90456308+Hossamb1@users.noreply.github.com>
2022-09-23 02:58:22 +0200

    Main Files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Weather-Journal
+
+## Description
+
+This project is an example of how to set
+
+## Requirements fulfilled
+
+1- Local server runs and produces feedback to the Command Line through a working callback function. <br>
+2- GET route setup on the server side. <br>
+3- Asynchronous function to fetch the data from the app endpoint. <br>
+
+This is my first attempt on the Weather Journal project. <br>
+Template is made and designed by me, <br>
+**_- Hossam Barakat_**
---
+++ website/app.js
@@ -0,0 +1,58 @@
+// Base URL for OpenWeatherMap API
+let baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+// Personal API Key for OpenWeatherMap API
+let apiKey = "&units=metric&appid=7002ef44000918473a71116248f103e3";
+// Creating the fullDate
+const date = new Date();
+const fullDate = `${date.getDate()}/${
+  date.getMonth() + 1
+}/${date.getFullYear()}`;
+
+document.getElementById("generate").addEventListener("click", performAction);
+
+function performAction() {
+  const zipCode = document.getElementById("zip").value;
+  getURL(baseURL, zipCode, apiKey);
+}
+const getURL = async (baseURL, zipCode, key) => {
+  // The URL with the zipCode
+  const res = await fetch(baseURL + zipCode + key);
+  // Transform into JSON
+  const data = await res.json();
+  const feel = document.getElementById("feelings").value;
+  // Send Data that we got back to store it in the server
+  const response = await fetch("/addData", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ date: fullDate, temp: data.main.temp, feel }),
+  });
+
+  try {
+    // Transform into JSON
+    const newData = await response.json();
+    return retrieveData();
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const retrieveData = async () => {
+  const request = await fetch("/all");
+  try {
+    // Transform into JSON
+    const allData = await request.json();
+    console.log(allData);
+    // Write updated data to DOM elements
+    document.getElementById("temp").innerHTML = `temperature: ${Math.round(
+      allData.temp
+    )} degrees`;
+    document.getElementById("content").innerHTML = allData.feel;
+    document.getElementById("date").innerHTML = `Date: ${allData.date}`;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
